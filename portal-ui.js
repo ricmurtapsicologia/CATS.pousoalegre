3256684a15c4b0c47748cc9258aaa9e4651edd17
ricmurtapsicologia <164411318+ricmurtapsicologia@users.noreply.github.com>
2026-09-09 20:51:23 -0300

    Carrega restauração dos PPTs originais de Juiz de Fora

--- portal-ui.js
+++ portal-ui.js
@@ -9,4 +9,5 @@
   };
   load('https://ricmurtapsicologia.github.io/Curso-ATS/access-2026.js?v=20260909-1','access-2026.js');
   load('portal-ui-core.js?v=20260909-1','portal-ui-core.js');
+  load('presentation-originals.js?v=20260909-2','presentation-originals.js');
 })();
